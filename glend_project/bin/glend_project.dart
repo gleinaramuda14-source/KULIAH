0f78f64f30939368cc30ec5be65909159f9d1a5f
Glend Patrick <gleinaramuda14@gmail.com>
2025-08-24 00:16:53 +0900

    komit pertama

---
+++ glend_project/bin/glend_project.dart
@@ -0,0 +1,5 @@
+import 'package:glend_project/glend_project.dart' as glend_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${glend_project.calculate()}!');
+}
---
+++ glend_project/test/glend_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:glend_project/glend_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
